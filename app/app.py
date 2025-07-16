6184c59c82460cbe521a835b4e0362d67fd5e343
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-07-16 23:01:01 +0530

    upd: added t2sl dataset in sidebar

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from imports import *
 from config import *
-# from pages.text2sign.t2sl import t2sl
+from pages.text2sign.t2sl import t2sl
 from pages.text2sign.t2s_2 import t2s_2
 # from pages.sign2text.func2 import s2t
 from pages.sign2text.func3 import s2t
@@ -17,6 +17,7 @@ def intro():
     ## Welcome to the Text2Sign Application
 
     In the sidebar, you will find three modules:
+    - **T2S Dataset (Text 2 Sign : Dataset Search): Finds Matching Sign Language .gif for given Text from ISL Dataset**
     - **T2SL (Text to Sign Language):** Convert written text into sign language representations.
     - **SL2T (Sign Language to Text):** Translate sign language gestures or videos into written text.
     - **S2S (Sign to Sign):** Transform one sign language format or style into another.
@@ -27,6 +28,7 @@ def intro():
 
 page_names = {
     "Home" : intro,
+    "T2S_from_Dataset" : t2sl,
     "T2SL" : t2s_2,
     "SL2T" : s2t,
     "S2S" : s2s,
